356c65111459fb33299fd7cc2b4fced8b913da3b
Vitaly Ustsilouski <vitaliustsilouski@gmail.com>
2020-03-06 00:53:58 +0300

    complete: resolve task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    return array === undefined || array.length === 0 ? 0:  Math.min(...array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    return array === undefined || array.length === 0 ?  0 :  Math.max(...array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0 ) {
+        return 0;
+    }
+
+    const result = array.reduce((acc, el, index) => {
+        return acc + el;
+    }, 0);
+    const number = array.length;
+
+    return result / number;
 }
